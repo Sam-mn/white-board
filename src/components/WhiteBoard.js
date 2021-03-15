6bfba90c32e8aafbaf2b6ae6cd8107051f145315
samer munawwar <samer.mnr@gmail.com>
2021-03-15 22:34:31 +0100

    added color and size

--- src/components/WhiteBoard.js
+++ src/components/WhiteBoard.js
@@ -1,4 +1,21 @@
 import React, { useRef, useState, useEffect } from "react";
+import styled from "styled-components";
+
+const ColorsDiv = styled.div`
+    padding: 0.5rem 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-color: #565555;
+`;
+
+const SizeDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 0.5rem;
+`;
 
 const WhiteBoard = () => {
     const canvasRef = useRef();
@@ -19,6 +36,18 @@ const WhiteBoard = () => {
         context.lineCap = "round";
         contextRef.current = context;
     }, []);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        context.strokeStyle = color;
+    }, [color]);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        context.lineWidth = lineSize;
+    }, [lineSize]);
 
     const startDraw = ({ nativeEvent }) => {
         const { offsetX, offsetY } = nativeEvent;
@@ -42,8 +71,29 @@ const WhiteBoard = () => {
         const data = canvasRef.current.toDataURL("image/png");
     };
 
+    const handleChangeColor = (e) => {
+        setColor(e.target.value);
+    };
+
+    const handleOnChange = (e) => {
+        setLineSize(e.target.value);
+    };
+
     return (
         <div>
+            <ColorsDiv className='colors'>
+                <SizeDiv>
+                    <select name='size' id='size' onChange={handleOnChange}>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                        <option value='6'>6</option>
+                    </select>
+                </SizeDiv>
+                <input type='color' onChange={handleChangeColor} />
+            </ColorsDiv>
             <canvas
                 onMouseDown={startDraw}
                 onMouseUp={stopDraw}
