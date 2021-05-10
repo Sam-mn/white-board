0a8aa60be23965b4417c7ff3ae913640efe0b7d5
samer munawwar <samer.mnr@gmail.com>
2021-05-10 16:45:08 +0200

    fix

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDVgH7mOMehNkrFACDMzdObYtD50mqqmm0",
-    authDomain: "awesome-todos-7b1b6.firebaseapp.com",
-    databaseURL: "https://awesome-todos-7b1b6.firebaseio.com",
-    projectId: "awesome-todos-7b1b6",
-    storageBucket: "awesome-todos-7b1b6.appspot.com",
-    messagingSenderId: "971662902753",
-    appId: "1:971662902753:web:5b95427911f6d1de617ea0",
+    databaseURL: process.env.DATA_BASE_URL,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
