35ae996e6eb06186f01c26200ab2ec72be3b1551
samer munawwar <samer.mnr@gmail.com>
2021-05-11 17:41:10 +0200

    fix

--- client/src/modules/socket-clint.js
+++ client/src/modules/socket-clint.js
@@ -1,6 +1,7 @@
 import socketIOClient from "socket.io-client";
 
-const endpoint = "http://localhost:8000/";
+// const endpoint = "http://localhost:8000/";
+const endpoint = "https://a-digital-whiteboard.herokuapp.com/";
 
 const socket = socketIOClient(endpoint);
 
