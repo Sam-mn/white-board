072a844bc66670235f08389475943e0a4e0cc88e
samer munawwar <samer.mnr@gmail.com>
2021-05-11 11:13:21 +0200

    fix

--- server/app.js
+++ server/app.js
@@ -3,12 +3,19 @@
  */
 
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 app.use(require("cors")());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", require("./router"));
+
+// app.use("/", require("./router"));
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
